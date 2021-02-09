5448e87e40cd5288d91e811cbeae8509c4c21a36
Thasciano Lima Carvalho <thasciano@gmail.com>
2021-02-09 11:00:39 -0300

    infra

--- lib/modules/search/domain/errors/errors.dart
+++ lib/modules/search/domain/errors/errors.dart
@@ -1,6 +1,9 @@
-abstract class FailureSearch implements Exception {
-}
+abstract class FailureSearch implements Exception {}
 
-class InvalidTextError implements FailureSearch{
-  
+class InvalidTextError implements FailureSearch {}
+
+class DatasourceError implements FailureSearch {
+  final String message;
+
+  DatasourceError({this.message});
 }
